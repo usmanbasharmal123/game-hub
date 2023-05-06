a6e5b3c257e4d7cac05c4432727875b0ebfb614e
Basharmal <usman.basharmal123@gmail.com>
2023-05-06 13:40:39 +0430

    Create a generi data fetch

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,43 +1,8 @@
-import React, { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+import useData from "./useData";
 export interface Genre {
   id: number;
   name: string;
-  //   background_image: string;
-  //   parent_platforms: { platform: Platform }[];
-  //   metacritic: number;
 }
-interface FetchGenresResponse {
-  count: number;
-  results: Genre[];
-}
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true);
 
-    apiClient
-      .get<FetchGenresResponse>("/genres", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-    return () => controller.abort();
-  }, []);
-  return { genres, error, isLoading };
-};
+const useGenres = () => useData<Genre>("/genres");
 export default useGenres;
