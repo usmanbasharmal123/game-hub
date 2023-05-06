d06cf0c5d2f87ffa127d69efc5da07aee940c20b
Basharmal <usman.basharmal123@gmail.com>
2023-05-06 19:51:58 +0430

    Build search input

--- src/components/ColorModeSwtich.tsx
+++ src/components/ColorModeSwtich.tsx
@@ -10,7 +10,7 @@ const ColorModeSwtich = () => {
           isChecked={colorMode === "dark"}
           onChange={toggleColorMode}
         />
-        <Text>Dark Mode</Text>
+        <Text whiteSpace="nowrap">Dark Mode</Text>
       </HStack>
     </>
   );
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../../assets/logo.webp";
+import logo from "../../assets/react.svg";
 import ColorModeSwtich from "../ColorModeSwtich";
+import SearchInput from "../SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="40px" />
+      <SearchInput />
       <ColorModeSwtich />
     </HStack>
   );
