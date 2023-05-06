050968a49b1d4b0ea69029e0eb31a723a8048c19
Basharmal <usman.basharmal123@gmail.com>
2023-05-06 19:33:33 +0430

    Sort the games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,25 +1,38 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 
 import { BsChevronDown } from "react-icons/bs";
-
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average rating" },
+  ];
+  const currentSortOrder = sortOrders.find(
+    (order) => order.value === sortOrder
+  );
   return (
     <>
-      {" "}
       <Menu>
         <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-          Orderby:Relevance
+          Order by : {currentSortOrder?.label || " Relevance"}
         </MenuButton>
         <MenuList>
-          {/* {data.map((platform) => ( */}
-          <MenuItem>
-            {/* //   onClick={() => onSelectPlatform(platform)}
-            //   key={platform.id}
-            // >
-            //   {platform.name} */}
-            Relevance
-          </MenuItem>
-          {/* ))} */}
+          {sortOrders.map((order) => (
+            <MenuItem
+              onClick={() => onSelectSortOrder(order.value)}
+              key={order.value}
+              value={order.value}
+            >
+              {order.label}
+            </MenuItem>
+          ))}
         </MenuList>
       </Menu>
     </>
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,5 @@
 const getCroppedImagerUrl = (url: string) => {
+  if (!url) return "";
   const target = "media/";
   const index = url.indexOf(target) + target.length;
 
