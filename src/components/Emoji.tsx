3e620cf209172580c241088a665793449fc640e3
Basharmal <usman.basharmal123@gmail.com>
2023-05-07 10:03:11 +0430

    Add emojis

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,19 @@
+import bullsEys from "../assets/bulls-eys.png";
+import thumbsUp from "../assets/thumbs-up.png";
+import meh from "../assets/meh.png";
+import { ImageProps, Image } from "@chakra-ui/react";
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh", boxSize: "25px" },
+    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+    5: { src: bullsEys, alt: "exceptional", boxSize: "35px" },
+  };
+  return <Image {...emojiMap[rating]} marginTop={1} />;
+};
+
+export default Emoji;
